0b92370844909c1856fb7a2e8066fb0ceed7ce85
Ivo-jose <ivojose19@gmail.com>
2021-10-28 18:16:36 -0300

    Check logic

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -27,12 +27,12 @@ public class ChessPosition {
 	}
 
 	//Class methods
-	protected Position toPosotion() {
+	protected Position toPosition() {
 		return new Position(8 - row, column - 'a');
 	}
 	
 	protected static ChessPosition fromPosition(Position position) {
-		return new ChessPosition((char) ('a' - position.getColumn()), 8 - position.getRow());
+		return new ChessPosition((char) ('a' + position.getColumn()), 8 - position.getRow());
 	}
 	
 	@Override
